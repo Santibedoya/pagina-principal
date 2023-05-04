4ef856f4357bf515deb097780724a55e7530af1b
Santiago <fuentesbedoyas@gmail.com>
2023-05-03 20:08:33 -0500

    Version taller

---
+++ js/js.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('botton')
+
+ function saludo(){
+
+      alert('Su ayuda ha sido recibida en momento nos pondremos en contacto')
+ }  
+ btn.addEventListener("click", saludo)
